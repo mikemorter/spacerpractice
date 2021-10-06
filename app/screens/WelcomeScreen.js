7b16450d3bce136da7299e209ed4b73bb00e425b
Mike Morter <mikemorter@gmail.com>
2021-10-06 11:03:16 -0400

    first commit

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../Images/SpacerLogoWords.png")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    position: "absolute",
+    width: 180,
+    height: 70,
+  },
+});
+
+export default WelcomeScreen;
